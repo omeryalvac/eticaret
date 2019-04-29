dd4ec80fbbad24654b6dacfaac28b4f6570c042b
omeryalvac <30379208+omeryalvac@users.noreply.github.com>
2019-04-30 00:54:01 +0300

    müşteri template kategori sayfası
    müşteri template kategori sayfası eklendi ve veritabanında kayıtlı kategoriler sidebar'a çekildi. Sidebar düzenlendi . İndex.php düzenlenmesi gerçekleştirildi.

--- sidebar.php
+++ sidebar.php
@@ -1,58 +1,57 @@
-	<div class="col-md-3"><!--sidebar-->
-				<div class="title-bg">
-					<div class="title">Categories</div>
+<div class="col-md-3"><!--sidebar-->
+	<div class="title-bg">
+		<div class="title">Kategoriler</div>
+	</div>
+	
+	<div class="categorybox">
+		<ul>
+
+			<?php 
+			$kategorisor=$db->prepare("SELECT * FROM kategori order by kategori_sira ASC");
+			$kategorisor->execute();
+			while($kategoricek=$kategorisor->fetch(PDO::FETCH_ASSOC)) { ?>
+			
+			<li><a href="kategori-<?=seo($kategoricek["kategori_ad"]) ?>"><?php echo $kategoricek['kategori_ad'] ?></a></li>
+
+			<?php } ?>
+		</ul>
+	</div>
+
+
+
+	<!-- Kategoriler yukarıda -->
+
+	<div class="ads">
+		<a href="product.htm"><img src="images\ads.png" class="img-responsive" alt=""></a>
+	</div>
+
+	<div class="title-bg">
+		<div class="title">Best Seller</div>
+	</div>
+	<div class="best-seller">
+		<ul>
+			<li class="clearfix">
+				<a href="#"><img src="images\demo-img.jpg" alt="" class="img-responsive mini"></a>
+				<div class="mini-meta">
+					<a href="#" class="smalltitle2">Panasonic M3</a>
+					<p class="smallprice2">Price : $122</p>
 				</div>
-				
-				<div class="categorybox">
-					<ul>
-						<li><a href="category.htm">Women Accessories</a></li>
-						<li><a href="category.htm">Men Shoes</a></li>
-						<li><a href="category.htm">Gift Specials</a></li>
-						<li><a href="category.htm">Electronics</a>
-							<ul>
-								<li><a href="#">iPhone 4S</a></li>
-								<li><a href="#">Samsung Galaxy</a></li>
-								<li><a href="#">MacBook Pro 17"</a></li>
-							</ul>
-						</li>
-						<li><a href="category.htm">On sale</a></li>
-						<li><a href="category.htm">Summer Specials</a></li>
-						<li><a href="category.htm">Electronics</a></li>
-						<li class="lastone"><a href="category.htm">Unique Stuff</a></li>
-					</ul>
+			</li>
+			<li class="clearfix">
+				<a href="#"><img src="images\demo-img.jpg" alt="" class="img-responsive mini"></a>
+				<div class="mini-meta">
+					<a href="#" class="smalltitle2">Panasonic M3</a>
+					<p class="smallprice2">Price : $122</p>
 				</div>
-				
-				<div class="ads">
-					<a href="product.htm"><img src="images\ads.png" class="img-responsive" alt=""></a>
+			</li>
+			<li class="clearfix">
+				<a href="#"><img src="images\demo-img.jpg" alt="" class="img-responsive mini"></a>
+				<div class="mini-meta">
+					<a href="#" class="smalltitle2">Panasonic M3</a>
+					<p class="smallprice2">Price : $122</p>
 				</div>
-				
-				<div class="title-bg">
-					<div class="title">Best Seller</div>
-				</div>
-				<div class="best-seller">
-					<ul>
-						<li class="clearfix">
-							<a href="#"><img src="images\demo-img.jpg" alt="" class="img-responsive mini"></a>
-							<div class="mini-meta">
-								<a href="#" class="smalltitle2">Panasonic M3</a>
-								<p class="smallprice2">Price : $122</p>
-							</div>
-						</li>
-						<li class="clearfix">
-							<a href="#"><img src="images\demo-img.jpg" alt="" class="img-responsive mini"></a>
-							<div class="mini-meta">
-								<a href="#" class="smalltitle2">Panasonic M3</a>
-								<p class="smallprice2">Price : $122</p>
-							</div>
-						</li>
-						<li class="clearfix">
-							<a href="#"><img src="images\demo-img.jpg" alt="" class="img-responsive mini"></a>
-							<div class="mini-meta">
-								<a href="#" class="smalltitle2">Panasonic M3</a>
-								<p class="smallprice2">Price : $122</p>
-							</div>
-						</li>
-					</ul>
-				</div>
-				
+			</li>
+		</ul>
+	</div>
+
 			</div><!--sidebar-->
