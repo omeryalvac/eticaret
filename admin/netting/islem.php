60107ce98cd826df675a3f011956b893eb6a0798
omeryalvac <30379208+omeryalvac@users.noreply.github.com>
2019-05-04 19:16:29 +0300

    Yorum Onay
    Yorum onaylama işlemi yapıldı..

--- admin/netting/islem.php
+++ admin/netting/islem.php
@@ -980,6 +980,37 @@ if (isset($_POST['iletisimayarkaydet'])) {
                             
                             }
 
+                            if ($_GET['yorum_onay']=="ok") {
+
+	
+
+	
+                                $duzenle=$db->prepare("UPDATE yorum SET
+                                    
+                                    yorum_onay=:yorum_onay
+                                    
+                                    WHERE yorum_id={$_GET['yorum_id']}");
+                                
+                                $update=$duzenle->execute(array(
+                            
+                            
+                                    'yorum_onay' => $_GET['yorum_one']
+                                    ));
+                            
+                            
+                            
+                                if ($update) {
+                            
+                                    
+                            
+                                    Header("Location:../production/yorum.php?durum=ok");
+                            
+                                } else {
+                            
+                                    Header("Location:../production/yorum.php?durum=no");
+                                }
+                            
+                            }
 
 
                             if($_GET[yorumsil]=="ok") {
