391ee0a48a4b2b1236de399d4658271c5f8fc26a
ugurcan93 <12542541@firat.edu.tr>
2019-05-30 02:56:07 +0300

    Update islem.php
    urun foto silme özelligi eklendi

--- admin/netting/islem.php
+++ admin/netting/islem.php
@@ -1388,4 +1388,33 @@ if (isset($_POST['bankasiparisekle'])) {
 
 
 }
+
+
+if(isset($_POST['urunfotosil'])) {
+
+	$urun_id=$_POST['urun_id'];
+
+
+	echo $checklist = $_POST['urunfotosec'];
+
+	
+	foreach($checklist as $list) {
+
+		$sil=$db->prepare("DELETE from urunfoto where urunfoto_id=:urunfoto_id");
+		$kontrol=$sil->execute(array(
+			'urunfoto_id' => $list
+			));
+	}
+
+	if ($kontrol) {
+
+		Header("Location:../production/urun-galeri.php?urun_id=$urun_id&durum=ok");
+
+	} else {
+
+		Header("Location:../production/urun-galeri.php?urun_id=$urun_id&durum=no");
+	}
+
+
+} 
 ?>
